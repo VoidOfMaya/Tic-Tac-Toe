41e978dbc77f4ff67ead050f038f8237e2d6ecbb
VoidOfMaya <mayaofthevoid@gmail.com>
2025-01-10 03:21:58 +0200

    implementsplayer object with basic functionality intagration

--- main.js
+++ main.js
@@ -3,10 +3,25 @@ console.log("im working");
 //main gameboard
 const Gameboard = (function(){
     
-// corisponding index:= [1,2,3,4,5,6,7,8,9] this is index on a 3x3 grid starting from 1
+// co-responding index:=[1,2,3,4,5,6,7,8,9] this is index on a 3x3 grid starting from 1
     let gameboardArr =  [0,0,1,0,0,1,0,0,1];
 
-    return { gameboardArr,};
+    //creating players
+    const playerOne = Player(gameboardArr);
+    playerOne.Setmarker('x');
+
+    const playerTwo = Player(gameboardArr);
+    playerTwo.Setmarker('o');
+
+    //playerTwo.OccupiePosition(4);
+    //playerOne.OccupiePosition(3);
+
+    
+    console.log(`Player one: ${playerOne.DisplayMarker()},\nplayer two: ${playerTwo.DisplayMarker()}`)
+
+    
+    GameFlow(playerOne,playerTwo);
+    //Checkwinner(gameboardArr);
 }());
 
 function Checkwinner(gameArray){
@@ -37,5 +52,87 @@ function Checkwinner(gameArray){
 
 }
 
-console.log(Gameboard);
-Checkwinner(Gameboard.gameboardArr);
+function GameFlow(playerX, playerO){
+
+    let gameOver = false;
+    let playerTurn = "";
+    let turnCount =0;
+
+    if(turnCount == 0){
+        console.log('Welcome to Tic-Tac-Toe, to start a new game choos your team\n');
+        let myTeam = ChooseTeam();
+        switch (true){
+            case (myTeam === "x" && gameOver === false):
+                console.log("you have entered the X block");
+                playerTurn = "x";
+                while( gameOver===false || turnCount < 9){
+                    if(playerTurn==="x"){
+                        playerX.OccupiePosition(prompt('choose a position for X on the bored to occupie'));
+                        playerTurn = "o";
+                        turnCount + 1                    }
+                    if(playerTurn==="o" || turnCount < 9){
+                        playerO.OccupiePosition(prompt('choose a position for O on the bored to occupie'));
+                        playerTurn = "x";
+                        turnCount + 1
+                    }
+                }
+                break;
+            case(myTeam ==="o" && gameOver === false):
+                playerTurn = "o";
+                while( gameOver===false){
+                    if(playerTurn==="o"){
+                        playerX.OccupiePosition(prompt('choose a position for O on the bored to occupie'));
+                        playerTurn = "x";
+                    }
+                    if(playerTurn==="x"){
+                        playerO.OccupiePosition(prompt('choose a position for X on the bored to occupie'));
+                        playerTurn = "o";
+                    }
+            }
+                console.log("you have entered the O block");
+
+                break;
+            case(gameOver === true):
+                console.log("you have entered the end game block, Announce winner");
+                breaak;
+            default:
+                console.log('dead end here bud');
+                break;
+        }
+    }
+    function ChooseTeam(){
+        return prompt("Choose your player by entering x or o, then press enter").toLocaleLowerCase();
+    }
+
+
+}
+//factory function for player creation takes gameboard array so it can interact with it
+function Player(gameboardArray){
+
+    let marker = 'none';
+    let internalArr = []
+    let winCount = 0;
+
+    function Setmarker(markerChoice){
+        marker = markerChoice;
+    }
+
+    function DisplayMarker(){
+        return marker;
+    }
+
+    function OccupiePosition(positionChoice){
+
+        if (gameboardArray[positionChoice] !== 'x' || gameboardArray[positionChoice] !== 'o'){
+            gameboardArray.splice(positionChoice, 1, marker);
+        }
+    }
+
+
+    return{ Setmarker, 
+            DisplayMarker, 
+            OccupiePosition,}
+}
+
+//console.log(Gameboard);
+//Checkwinner(gameboardArr);
