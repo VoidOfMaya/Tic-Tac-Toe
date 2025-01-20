0a67e928ecd8a9e5c860bc136b92ea13d4145ebb
VoidOfMaya <mayaofthevoid@gmail.com>
2025-01-20 21:00:06 +0200

    reorgenizes code for more readability,a function to manage player turn is still non functional

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const Gameboard = (function () {
 
     //populates gameboard array with relevant dom object
     let gameboardArr = ["", "", "", "", "", "", "", "", ""];
+    //player turn boverner 
+    let turnStatus = 0;
     //gameboardArr.push(document.getElementById(`${i}`));
     let elementArray = gameboardArr.map((id, index) => document.getElementById(index));
     //creates players
@@ -21,8 +23,13 @@ const Gameboard = (function () {
     }
 
     return {
+        gameboardArr,
         playerOne,
         playerTwo,
+        //displays player marker  on chosen segmant in gameboard
+        Display(id, playerMarker) {
+            id.innerText = playerMarker;
+        },
         //returns relevant dom object from gamebaord based on players click
         Position(playermarker) {
             const element = elementArray;
@@ -41,20 +48,15 @@ const Gameboard = (function () {
 
                 playerOne.Setmarker('x');
                 playerTwo.Setmarker('o');
-                //console.log(`starting player ${playerOne.GetMarker()}\nand secondin turn is ${playerTwo.GetMarker()}`)
                 dialog.close();
-                dialog.style.display = 'none';
-                GameLogic.GameFlow(playerOne, playerTwo);
-
+                dialog.style.display = 'none';             
             })
             selectOPlayerBtn.addEventListener('click', () => {
 
                 playerOne.Setmarker('o');
                 playerTwo.Setmarker('x');
-                //console.log(`marker has been choosen: ${playerOne.GetMarker()}`);
                 dialog.close();
                 dialog.style.display = 'none';
-                GameLogic.GameFlow(playerOne, playerTwo);
             })
         }
     }
@@ -85,23 +87,80 @@ const Gameboard = (function () {
 }());
 const GameLogic = (function () {
     return {
+        //makes it so each player can take turn playing the game 
+        GameFlow(playerOne, playerTwo,playerTurn) {
 
-        //converts player array to a readable form
-        ReadArr(array) {
-            let playerArr = array.GetInnerArray();
-            for (let i = 0; i < playerArr.length; i++) {
-                if (playerArr[i] == "") {
-                    playerArr.splice(i, 1, 0);
+            const gameboardBtns = document.querySelectorAll('.inner-gameboard');
+            Array.from(gameboardBtns).map((btn, index) => {
+                if(playerTurn === 0){
+                    btn.addEventListener('click', ()=>{
+                        playerOne.OccupiePosition(index);
+                        playerOne.Checkwinner();
+                        console.log(playerOne.GetArray() + playerOne.GetMarker());
+                        return 1;
+                    })
                 }
-                if (playerArr[i] == 'x' || playerArr[i] == 'o') {
-                    playerArr.splice(i, 1, 1);
+                if(playerTurn === 1){
+                    btn.addEventListener('click', ()=>{
+                        playerTwo.OccupiePosition(index);
+                        playerTwo.Checkwinner();
+                        console.log(playerTwo.GetArray() + playerTwo.GetMarker());
+                        return 0;
+                    })
                 }
-            }
-            return playerArr
+
+            })
         },
-        Checkwinner(player) {
+    }
+}());
+//factory function for player creation takes gameboard array so it can interact with it
+function Player(gameboardArray) {
+    let marker = 'none';
+    let innerArray = ["", "", "", "", "", "", "", "", ""]
 
-            const playerArr = this.ReadArr(player);
+    //converts each array from blank space to a 0 and player marker to a 1 at index
+    const readArray = (array) =>{
+        array.map((position, index) => {
+            if (position === "") {
+                array.splice(index, 1, 0)
+            }
+            if (position === "x" || position === "o") {
+                array.splice(index, 1, 1);
+            }
+        })
+        return array;
+    }
+    
+    return {
+        //makes player marker changable outside of scope
+        Setmarker(markerChoice) {
+            marker = markerChoice;
+        },
+        //makes player marker readably outside of scope
+        GetMarker() {
+            return marker;
+        },
+        //makes player array readable outside of scope
+        GetArray(){
+            return innerArray;
+        },
+        // takes a players position index and inputs marker to the coresponding position on the gameboard array
+        OccupiePosition(positionChoice) {
+
+            //console.log(`first gameboard array: ${gameboardArray}`)
+
+            if (gameboardArray[positionChoice] == "x" && gameboardArray[positionChoice] == "o") {
+                console.log(`you are trying to occupie position: ${positionChoice}\n in gameboard array: ${gameboardArray}`);
+                console.log("Action youre trying to make is not possible, This position is already occupied!")
+
+            }
+            if (gameboardArray[positionChoice] == "") {
+                gameboardArray.splice(positionChoice, 1, marker);
+                innerArray.splice(positionChoice, 1, marker);
+                Gameboard.Position(marker);                
+            }
+        },
+        Checkwinner() {
             const winningArr = [[1, 1, 1, 0, 0, 0, 0, 0, 0], //1
             [0, 0, 0, 1, 1, 1, 0, 0, 0], //2
             [0, 0, 0, 0, 0, 0, 1, 1, 1], //3
@@ -111,83 +170,28 @@ const GameLogic = (function () {
             [1, 0, 0, 0, 1, 0, 0, 0, 1], //7
             [0, 0, 1, 0, 1, 0, 1, 0, 0]];//8 possible winn positions 
 
+            //converts array to a readable format
+            let boardArray = readArray(gameboardArray);
             //function for comparing the array with each winning position:
-            for (let i = 0; i < winningArr.length;) {
-                //console.log(`winning position ${winningArr[i]}`);
-                var currentWinArr = winningArr[i].toString();
-                if (currentWinArr === playerArr.toString()) {
-                    console.log(`we have a winner at position ${i} `);
-                    i++;
+            for(let i = 0; i < winningArr.length; i++){
+                if (winningArr[i].toString() === boardArray.toString()) {
+                    console.log(`we have a winner `);
+                    break;
                 }
                 else {
                     console.log(`no winner here`);
-                    i++;
-                }
-
+                }               
             }
-            console.log(`gameboard: ${playerArr}`)
-        },
-        GameFlow(playerOne, playerTwo) {
-            let playerTurn = 0;
-            const X = playerOne.GetMarker();
-            const O = playerTwo.GetMarker();
-            console.log(`players are : 1.${X}\n2.${O}`);
+            console.log(boardArray);
 
-            const gameboardBtns = document.querySelectorAll('.inner-gameboard');
-            Array.from(gameboardBtns).map((btn, index) =>{
-                btn.addEventListener('click', ()=>{
-                    //console.log(btn);
-                    if(playerTurn === 0){
-                        playerOne.OccupiePosition(index);
-                        
-                    }
-                    if(playerTurn === 1){
-                        playerTwo.OccupiePosition(index);
-                        playerTurn = 0;
-                    }
-                })
-                
-            })
-        },
-    }
-}());
-//factory function for player creation takes gameboard array so it can interact with it
-function Player(gameboardArray) {
-    let marker = 'none';
-    let innerArray = ["", "", "", "", "", "", "", "", ""]
-    return {
-        //get set methods for marker
-        Setmarker(markerChoice) {
-            marker = markerChoice;
-        },
-        GetMarker() {
-            return marker;
-        },
-        //set for internal array =>this keeps track of game progress
-        GetInnerArray() {
-            return innerArray
-        },
-        // takes a players position index and inputs marker to the coresponding position on the gameboard array
-        OccupiePosition(positionChoice) {
-
-            //console.log(`first gameboard array: ${gameboardArray}`)
-            if (innerArray[positionChoice] == "") {
-                gameboardArray.splice(positionChoice, 1, marker);
-                innerArray.splice(positionChoice, 1, marker);
-                console.log(`second gameboard array: ${gameboardArray}`)
-                
-            }
-            if(gameboardArray[positionChoice] == "x" ||gameboardArray[positionChoice] == "o" ){
-                console.log(`you are trying to occupie position: ${positionChoice}\n in gameboard array: ${gameboardArray}`);
-                console.log("Action youre trying to make is not possible, This position is already occupied!")
-                return 0;
-            }
         }
     }
 }
 //GAME START
-const Game = function (playerOne, playerTwo) {
-
-    Gameboard.PlayerMarkerSelect();
+const Game = function (gameboard, govener, playerX, playerO) {
+    let turnStatus = 0;
+    gameboard.PlayerMarkerSelect();
+    govener.GameFlow(playerX,playerO, turnStatus);
+    
 }
-Game(Gameboard.playerOne, Gameboard.playerTwo);
+Game(Gameboard, GameLogic,Gameboard.playerOne, Gameboard.playerTwo);
